10a73ab1b925708082818cb655c0f8835c7cf60e
Dylan Graham <dylan.t.graham@gmail.com>
2023-03-09 22:35:30 -0600

    Add gold steps and requirements

---
+++ spark/gold/gold_analysis.py
@@ -0,0 +1,113 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder.appName('get-characters').getOrCreate()
+sc = spark.sparkContext
+
+ts = 1678335199  # int(time.time())
+characters_input = f"./case/silver/characters/uploaded_at={ts}/*.parquet"
+events_input = f"./case/silver/events/uploaded_at={ts}/*.parquet"
+
+gold_output_q1 = f"./case/gold/uploaded_at={ts}/q1/"
+os.makedirs(os.path.dirname(gold_output_q1), exist_ok=True)
+gold_output_q2 = f"./case/gold/uploaded_at={ts}/q2/"
+os.makedirs(os.path.dirname(gold_output_q2), exist_ok=True)
+gold_output_q3 = f"./case/gold/uploaded_at={ts}/q3/"
+os.makedirs(os.path.dirname(gold_output_q3), exist_ok=True)
+
+charactersDF = spark.read.parquet(characters_input)
+cDF = charactersDF.select(
+    'id',
+    'name',
+    'comics_available',
+    'events_available',
+    'stories_available',
+    'series_available'
+).orderBy(charactersDF.comics_available.desc())
+top10DF = spark.createDataFrame(cDF.orderBy(charactersDF.comics_available.desc()).take(10))
+top10_character_list = cDF.orderBy(charactersDF.comics_available.desc()).select('id').take(10)
+top10_ids = [str(x.id) for x in top10_character_list]
+
+# top10 = cDF.agg(F.collect_set('id'))
+###############
+eventsDF = spark.read.parquet(events_input)
+charEvDF = eventsDF.select(
+    F.explode(eventsDF.characters).alias('character'),
+    F.col('id').alias('event_id'),
+    'start',
+    'end',
+    F.datediff('end', 'start').alias('event_days')
+)
+top10_charEvDF = charEvDF.where(F.col('character.character_id').isin(top10_ids)).select(
+    'character.character_id',
+    'character.character_name',
+    'event_id',
+    'start',
+    'end',
+    # F.sequence(F.year(F.date_trunc('Year', 'start')), F.year(F.date_trunc('Year', 'end'))).alias('years'),
+    F.sequence(F.year('start'), F.year('end')).alias('years'),
+    'event_days'
+)
+
+top10_charEvAggDF = top10_charEvDF.groupby('character_id', 'character_name').agg(
+    F.array_distinct(F.flatten(F.collect_list('years'))).alias('event_years')
+    , F.sum('event_days').alias('total_event_days')
+    , F.collect_set('event_id').alias('event_ids')
+)
+
+###
+top10_joined = top10DF.join(top10_charEvAggDF, F.col('id') == F.col('character_id'), 'left')
+
+# Q1: Counts for the top 10 Character
+q1 = top10_joined.select(
+    'character_name',
+    'character_id',
+    'comics_available',
+    'events_available',
+    'stories_available',
+    'series_available',
+    'total_event_days'
+)
+q1.write.parquet(gold_output_q1, 'overwrite')
+
+# Q2: (Character, Year): Event count
+q2 = charEvDF.select(
+    'character.character_id',
+    F.explode(F.sequence(F.year('start'), F.year('end'))).alias('event_year')
+).groupby(
+    'event_year'
+).agg(
+    F.size(F.collect_set('character_id')).alias('distinct_characters')
+).orderBy(F.col('event_year'))
+q2.write.parquet(gold_output_q2, 'overwrite')
+
+# q2a = top10_charEvDF.select(
+#     'character_id',
+#     'character_name',
+#     'event_id',
+#     F.explode('years').alias('event_year')
+# ).groupby(
+#     'character_id', 'character_name', 'event_year'
+# ).agg(
+#     F.size(F.collect_set('event_id')).alias('distinct_events')
+# ).orderBy(F.col('character_name'), F.col('event_year'))
+
+# Q3: By year Dist count of chars
+q3 = top10_charEvDF.select(
+    F.explode('years').alias('event_year'),
+    'character_name',
+    'event_id'
+).groupby(
+    'character_name',
+    'event_year'
+).agg(
+    F.size(F.collect_set('event_id')).alias('distinct_events')
+).orderBy(
+    F.col('character_name'),
+    F.col('event_year')
+)
+q3.write.parquet(gold_output_q3, 'overwrite')
+
+
+
---
+++ spark/gold/plot_data.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+from plotly.offline import plot
+
+
+ts = 1678335199  # int(time.time())
+gold_q1 = f"./case/gold/uploaded_at={ts}/q1"
+gold_q2 = f"./case/gold/uploaded_at={ts}/q2"
+gold_q3 = f"./case/gold/uploaded_at={ts}/q3"
+
+plots = f"./case/gold/uploaded_at={ts}/plots"
+os.makedirs(os.path.dirname(plots), exist_ok=True)
+
+df_q1 = pd.read_parquet(gold_q1)
+df_q2 = pd.read_parquet(gold_q2)
+df_q3 = pd.read_parquet(gold_q3)
+
+
+q1a_plot = px.bar(df_q1, x='character_name',
+                  y=['comics_available', 'events_available', 'stories_available', 'series_available'],
+                  title='Top 10 Characters Appearances')
+q1b_plot = px.bar(df_q1, x='character_name', y='total_event_days', title='Top 10 Characters Total Event Days')
+
+q2_plot = px.line(df_q2, x='event_year', y='distinct_characters', title='Distinct Characters per Year')
+
+q3_plot = px.density_heatmap(df_q3, x="event_year", y="character_name", z='distinct_events', text_auto=True)
+
+figures = [
+    (q1a_plot, 1, 1),
+    (q1b_plot, 1, 2),
+    (q2_plot, 2, 1),
+    (q3_plot, 2, 2)
+]
+fig = make_subplots(rows=2, cols=2)
+
+for tup in figures:
+    (figure, r, c) = tup
+    for trace in range(len(figure["data"])):
+        fig.append_trace(figure["data"][trace], row=r, col=c)
+        # fig.
+
+fig.write_image(f"{plots}/dash.jpeg")
+plot(fig, filename=f"{plots}/dash2.jpeg")
+plot(fig)
+# fig.show()
+
+
+fig.show()
+# plotly.offline.plot(fig, filename='C:/plotlyplots/canada_offline.html')
+
